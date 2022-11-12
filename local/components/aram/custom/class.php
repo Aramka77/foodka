a766e18679c9d982ae89877245ad832bda6b7d8a
Aramka77 <aramka77@gmail.com>
2022-11-13 00:18:34 +0300

    new component

---
+++ local/components/aram/custom/class.php
@@ -0,0 +1,32 @@
+<?php
+
+class AramCustomComponent extends \CBitrixComponent{
+
+    private function includeModule(){
+        \Bitrix\Main\Loader::includeModule('iblock');
+    }
+
+    private function prepareResult(){
+        $this->arResult['element'] = \Bitrix\Iblock\Elements\ElementBannerAreaTable::getRow([
+            'select' => ['ID', 'NAME'],
+            'filter' => [
+                'ACTIVE' => 'Y'
+            ],
+            "cache" => ["ttl"=>3600, "cache_joins"=>true],
+        ]);
+
+    }
+    public function executeComponent()
+    {
+
+        if($this->startResultCache())//startResultCache используется не для кеширования html, а для кеширования arResult
+        {
+            $this->includeModule();
+            $this->prepareResult();
+            $this->includeComponentTemplate();
+        }
+
+    }
+
+
+}
